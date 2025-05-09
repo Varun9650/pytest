659adbb8da132ecd6ca15ec14b323e7b27cc7660
Varun Kaushik <87529836+Varun9650@users.noreply.github.com>
2025-05-09 15:35:29 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@
 #
 def factorial(number):
     if number < 0:
-        raise Exception("Negative input is there")
+        raise Exception("Negative input is here and there")
 
     if number <= 1:
         return 1
